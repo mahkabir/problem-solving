fe459391e100a03590b373c8e2429fc5e4daa345
Humayun Kabir <kabir.humayun@reddotdigitalit.com>
2024-09-07 23:48:47 +0600

    re order list

---
+++ src/linkList/ReOrderList143.java
@@ -0,0 +1,19 @@
+package linkList;
+
+public class ReOrderList143 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode () {};
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
